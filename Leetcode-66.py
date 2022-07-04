d2c141a88be7d2b8b9ba7870573501e73ad00a5a
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2022-07-04 15:39:26 +0500

    Add commit

--- Leetcode-66.py
+++ Leetcode-66.py
@@ -1,12 +1,3 @@
 if __name__ == "__main__":
     digits = list(map(int,input().split()))
-    son = 0
-    son_len = len(digits)-1
-    for i in digits:
-        son+= i*pow(10,son_len)
-        son_len-=1
-    son+=1
-    digits.clear()
-    for i in str(son):
-        digits.append(i)
-    print(digits)
+    print(list(str(i) for i in str(int(''.join([str(i) for i in digits]))+1)))
